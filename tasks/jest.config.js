acb5dfe6e8b8538e226c59ec1edfc6270c264099
Kevin Old <kevin@kevinold.com>
2022-09-01 15:06:59 -0500

    make additional adjustments to try to get jest typescript working

--- tasks/jest.config.js
+++ tasks/jest.config.js
@@ -4,8 +4,9 @@ module.exports = {
   transform: {
     '^.+\\.ts?$': 'ts-jest'
   },
+  transformIgnorePatterns: []
   //transformIgnorePatterns: ['<rootDir>/node_modules/']
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!(unist-util-visit-parents))'
-  ]
+  // transformIgnorePatterns: [
+  //   '<rootDir>/node_modules/(?!(unist-util-visit|unist-util-visit-parents))'
+  // ]
 };
